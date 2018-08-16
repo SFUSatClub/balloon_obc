5b497d5966d7f8db9849a95125ad684452ff6fe6
Kyle Smolko <ksmolko2@gmail.com>
2018-08-16 15:59:00 -0700

    Changed TX and init functions to allow packet transmissions and incorporate the TNC

--- src/Radio/Radio.cpp
+++ src/Radio/Radio.cpp
@@ -5,17 +5,24 @@ Radio::Radio()
 
 void Radio::init()
 {
-    // Initialize all of the RS-UV3's parameters
-    radioSerial.println("CLVA7KSX");
-    radioSerial.println("FS146520");
-
-
+    // Initialize all of the RS-UV3's and TNC's parameters
+    tncSerial.print("\n\n\n");
+    delay(500);
+    radioSerial.println("FS144390");
+    tncSerial.print("MYCALL VA7KSX\n");
+    delay(50);
+    tncSerial.print("POSITION GPS\n");
 }
 
-// Transmits unmodulated CW signal (morse code)
-void Radio::tx(String data)
+// Transmits APRS packet
+void Radio::txPacket()
 {
-    // Still deciding whether to send CW or MCW
-    radioSerial.println("CW" + data);
-    //radioSerial.println("CT" + data);
+    // Enables Radio's TX, gives time to stabilize, sends beacon, gives time
+    // for beacon to finish, then disable's Radio's TX
+    // Test these delays for the lowest value needed for reliable transmission
+    radioSerial.println("TX1");
+    delay(500);
+    tncSerial.print("BEACON\n");
+    delay(1500);
+    radioSerial.println("TX0");
 }
