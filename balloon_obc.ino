84857ab624adcb421f2092f669cf23322f95df74
kabirb <kabirb@sfu.ca>
2018-08-15 21:09:30 -0700

    Added SD card to obc file, basic sd capabilities

--- balloon_obc.ino
+++ balloon_obc.ino
@@ -7,6 +7,18 @@
 #include "src/Computing/HTU.h"
 #include "src/Computing/TempSensor.h"
 
+#include <SD.h>
+#include <SPI.h>
+File myFile;
+int pinCS = 53;
+
+//#include <SD.h>
+//#include <SPI.h>
+
+
+//int CS_PIN = 53;
+//File file;
+
 GPS gps;
 HTU htu;
 TempSensor lm75;
@@ -21,6 +33,62 @@ void setup() {
   gps.getVersion();
   htu.init();
   lm75.init();
+
+
+  //Serial.begin(9600);
+
+//  initializeSD();
+//  createFile("testing1.txt");
+//  writeToFile("Fucking SD card");
+//  closeFile();
+//  openFile("testing1.txt");
+//  Serial.println(readLine());
+//  Serial.println(readLine());
+//  closeFile();
+
+   
+  Serial.begin(9600);
+  pinMode(pinCS, OUTPUT);
+  
+  // SD Card Initialization
+  if (SD.begin())
+  {
+    Serial.println("SD card is ready to use.");
+  } else
+  {
+    Serial.println("SD card initialization failed");
+    return;
+  }
+  
+  // Create/Open file 
+  myFile = SD.open("test.txt", FILE_WRITE);
+  
+  // if the file opened okay, write to it:
+  if (myFile) {
+    Serial.println("Writing to file...");
+    // Write to file
+    myFile.println("Testing text 1, 2 ,3...");
+    myFile.close(); // close the file
+    Serial.println("Done.");
+  }
+  // if the file didn't open, print an error:
+  else {
+    Serial.println("error opening test.txt");
+  }
+  // Reading the file
+  myFile = SD.open("test.txt");
+  if (myFile) {
+    Serial.println("Read:");
+    // Reading the whole file
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+   }
+    myFile.close();
+  }
+  else {
+    Serial.println("error opening test.txt");
+  }
+
 }
 
 // Interrupt is called once a millisecond, looks for any new GPS data, and stores it
@@ -53,4 +121,95 @@ void loop() {
  
   lm75.tick();
   delay(3000);
+
+
 }
+
+
+//SD Card function Definitions...
+//void initializeSD()
+//{
+//  Serial.println("Initializing SD card...");
+//  pinMode(CS_PIN, OUTPUT);
+//
+//  if (SD.begin())
+//  {
+//    Serial.println("SD card is ready to use.");
+//  } else
+//  {
+//    Serial.println("SD card initialization failed");
+//    return;
+//  }
+//}
+//
+//int createFile(char filename[])
+//{
+//  file = SD.open(filename, FILE_WRITE);
+//
+//  if (file)
+//  {
+//    Serial.println("File created successfully.");
+//    return 1;
+//  } else
+//  {
+//    Serial.println("Error while creating file.");
+//    return 0;
+//  }
+//}
+//
+//int writeToFile(char text[])
+//{
+//  if (file)
+//  {
+//    file.println(text);
+//    Serial.println("Writing to file: ");
+//    Serial.println(text);
+//    return 1;
+//  } else
+//  {
+//    Serial.println("Couldn't write to file");
+//    return 0;
+//  }
+//}
+//
+//void closeFile()
+//{
+//  if (file)
+//  {
+//    file.close();
+//    Serial.println("File closed");
+//  }
+//}
+//
+//int openFile(char filename[])
+//{
+//  file = SD.open(filename);
+//  if (file)
+//  {
+//    Serial.println("File opened with success!");
+//    return 1;
+//  } else
+//  {
+//    Serial.println("Error opening file...");
+//    return 0;
+//  }
+//}
+//
+//String readLine()
+//{
+//  String received = "";
+//  char ch;
+//  while (file.available())
+//  {
+//    ch = file.read();
+//    if (ch == '\n')
+//    {
+//      return String(received);
+//    }
+//    else
+//    {
+//      received += ch;
+//    }
+//  }
+//  return "";
+//}
