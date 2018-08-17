166b9fb4cf96a3c9680f13525a58f7c423f185f2
Kyle Smolko <ksmolko2@gmail.com>
2018-08-17 03:17:01 -0700

    Enabled VOX on radio and calculated DD format for coordinates with GPS

--- src/Computing/GPS.cpp
+++ src/Computing/GPS.cpp
@@ -15,6 +15,8 @@ void GPS::init()
 
 }
 
+// Reads gps coordinates from the TNC and sets it as the current
+// position.
 void GPS::tick()
 {
     if (timer > millis()) timer = millis();
@@ -23,30 +25,40 @@ void GPS::tick()
     if (millis() - timer > 2000)
     {
         timer = millis(); // reset the timer
-
-        // This reads gps coordinates from the TNC and sets it as the current
-        // position. This is in DMS format, will later add conversion to DD
-        // format (for google maps purposes)
         tncSerial.print("POSITION\n");
         delay(50);
         while(tncSerial.available())
         {
             int inChar = Serial.read();
+            int temp = 0;
             if (isDigit(inChar) || (char)inChar == '.')
             {
                 position += (char)inChar;
             }
-            else if ((char)inChar == 'N')
+            else if ((char)inChar == 'N' || (char)inChar == 'S')
             {
                 latitude = position.toFloat();
+                temp = latitude;
+                latitude = (latitude-100*int(latitude/100))/60.0;
+                latitude += int(temp/100);
+                if((char)inChar == 'S')
+                {
+                    latitude = 0 - latitude;
+                }
                 position = "\0";
             }
-            else if ((char)inChar == 'W')
+            else if ((char)inChar == 'W' || (char)inChar == 'E')
             {
                 longitude = position.toFloat();
+                temp = longitude;
+                longitude = (longitude-100*int(longitude/100))/60.0;
+                longitude += int(temp/100);
+                if((char)inChar == 'W')
+                {
+                    longitude = 0 - longitude;
+                }
                 position = "\0";
             }
-
         }
     }
 }
--- src/Radio/Radio.cpp
+++ src/Radio/Radio.cpp
@@ -1,3 +1,9 @@
+/* TNC commands can be found here:
+ * http://www.argentdata.com/support/OpenTrackerUSB_manual.pdf
+ * RS-UV3 commands here:
+ * https://www.hobbypcb.com/files/download/2-rs-uv3/4-rs-uv3-user-manual
+ */
+
 #include "Radio.h"
 
 Radio::Radio()
@@ -9,11 +15,15 @@ void Radio::init()
     tncSerial.print("\n\n\n");
     delay(500);
     radioSerial.println("FS144390");
+    radioSerial.println("VX1");
+
     tncSerial.print("MYCALL VA7KSX\n");
     delay(50);
     tncSerial.print("POSITION GPS\n");
     delay(50);
     tncSerial.print("ALTITUDE ON\n");
+    delay(50);
+    tncSerial.print("TXDELAY 150\n");
     delay(50);
 
     // Clear incoming buffers
@@ -26,16 +36,8 @@ void Radio::init()
 // Transmits APRS packet
 void Radio::txPacket()
 {
-    // Enables Radio's TX, gives time to stabilize, sends beacon, gives time
-    // for beacon to finish, then disable's Radio's TX
-    // Test these delays for the lowest value needed for reliable transmission
-    radioSerial.println("TX1");
-    delay(500);
     tncSerial.print("BEACON\n");
     delay(1500);
-    radioSerial.println("TX0");
-    delay(50);
-
     // Clear incoming buffers
     while(tncSerial.available())
     {
